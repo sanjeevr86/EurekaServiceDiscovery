3b2555afbfbb3b45862df2503ac56939d8b27eba
sanjeevi <sanjeev.r86@gmail.com>
2018-08-30 22:18:10 +0530

    add app file for eureka server

--- src/main/java/com/eureka/service/api/EurekaServiceDiscoveryApplication.java
+++ src/main/java/com/eureka/service/api/EurekaServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.eureka.service.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
